857321085565cd038ed95de5e44eddf1e19c27e8
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-28 12:08:02 +0530

    Create amazon.java

---
+++ amazon.java
@@ -0,0 +1,15 @@
+/* BLC & ELC Program */
+class amazon
+{
+    int a,b,c,d;
+    void input()
+    {
+        a=20; b=10;
+    }
+    void show()
+    {
+        c=a+b;
+        d=a-b;
+        System.out.println(c+" "+d);
+    }
+}
