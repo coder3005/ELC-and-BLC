74288d5bb4c9a94027f01f9e144873bf213aaeec
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-28 12:09:58 +0530

    Create anish.java

---
+++ anish.java
@@ -0,0 +1,17 @@
+class anish
+{
+    public static  void main(String[] args) {
+
+        int n=20,m=10,rem;
+        rem=n%m;
+        System.out.println(rem);
+        
+        amazon r=new amazon();
+        r.input();
+        r.show();
+
+        flipkart r2=new flipkart();
+        r2.input();
+        r2.show();
+    }
+}
