4ceeba418c4744a6b04d974e02307c876f1deeba
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-28 12:09:30 +0530

    Create ankush.java

---
+++ ankush.java
@@ -0,0 +1,13 @@
+class ankush
+{
+    public static  void main(String[] args) {
+
+        amazon r=new amazon();
+        r.input();
+        r.show();
+
+        flipkart r2=new flipkart();
+        r2.input();
+        r2.show();
+    }
+}
